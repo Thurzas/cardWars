f2e9fdcc5e5b47f1e9dda1be9388812b94862064
Gozecode <romeo.teutsch@gmail.com>
2024-12-19 19:36:23 +0100

    home

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,7 +1,35 @@
+import "./Home.css";
+import "../../assets/fonts/font.css";
+import "../../assets/fonts/Starjedi.ttf";
+
 function Home() {
 	return (
 		<>
-			<h2>Hello, Home !</h2>
+			<main id="home_container">
+				<h1>Choisis ton camp !</h1>
+				<div id="hero_zero">
+					<img
+						className="vador"
+						src="/home-img/darkvador.png"
+						alt="darkvador"
+					/>
+					<img
+						className="anakin"
+						src="/public/home-img/hanakin.png"
+						alt="hanakin"
+					/>
+					<img
+						id="vador_booster"
+						src="/public/home-img/booster.png"
+						alt="booster"
+					/>
+					<img
+						id="anakin_booster"
+						src="/public/home-img/booster.png"
+						alt="booster"
+					/>
+				</div>
+			</main>
 		</>
 	);
 }
