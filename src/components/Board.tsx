0b6f123231cac4f78eb395847196ed9ae2502a47
mitsuri91 <launaymaxime5@gmail.com>
2024-12-19 20:59:56 +0100

    add card and svg

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import CardItem from '../utils.tsx';
+import CardItem from "../utils.tsx";
 
 //ouais c'est pas forcément attendu, mais j'ai une meilleur compréhension de la logique de jeu en paradigme Objet ^^'
-class BoardGame extends  Component {
-
-}
+class BoardGame extends Component {}
