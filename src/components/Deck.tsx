ad8c017b4ca17d32c56eafdbe3ecbeb6b86277b1
Thurzas <mathieu.miot@gmail.com>
2024-12-19 16:05:18 +0100

    first part of game logic

---
+++ src/components/Deck.tsx
@@ -0,0 +1,31 @@
+import {CardData,AssetsData,CardItem,DeckProps} from '../utils.tsx';
+import {useState} from  'react';
+
+
+function Deck(data:DeckProps){
+    const [DarkDeck, setDarkDeck] = useState([]);
+    const [LightDeck, setLightkDeck] = useState([]);
+    const mergedData = data.data.map(card =>{
+        const targetedAsset = data.assets.find(asset => asset.id === card.id);
+        if(targetedAsset)
+            return new CardItem(card.id,card.imageUrl,targetedAsset.title,targetedAsset.attack,targetedAsset.health,targetedAsset.cost);
+    })
+    
+    const cards = mergedData.filter(card => card !== undefined);
+    console.log(mergedData);
+   return(
+        <>
+           <h2>Deck show off</h2>
+           {cards.map(card =>(
+            card !== undefined?
+                <Card props={card}/>
+                :
+                <p>An error occured</p>
+           ))}
+        </>
+    );
+    
+}
+    
+
+export default Deck;
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,1 +1,39 @@
 //mettez les interfaces et les classes ici pour plus de netteté
+import {Component} from 'react';
+
+export interface CardData{
+    id:number;
+    name: string;
+    imageUrl: string;
+}
+export interface AssetsData{
+    id:number;
+    title: string;
+    attack: number;
+    health: number;
+    cost: number;
+}
+
+export interface DeckProps
+{
+    data:CardData[];
+    assets: AssetsData[];
+}
+
+
+export class CardItem{
+    id:number;
+    portrait:string;
+    title: string;
+    attack: number;
+    health: number;
+    cost: number;
+    constructor(id:number,portrait:string, title:string, attack:number, health:number, cost:number){
+        this.id = id;
+        this.portrait = portrait;
+        this.title = title;
+        this.attack = attack;
+        this.health = health;
+        this.cost = cost;                
+    }
+}
